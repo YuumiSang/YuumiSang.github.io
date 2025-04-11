5643636ccc1381c0584a88a67464463d6f46d335
Neo <117387756+DesireeGon@users.noreply.github.com>
2025-04-11 18:41:39 +0200

    wawa
    wi

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,8 +21,8 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
-  base: "/",
+  site: "https://yuumisang.github.io",
+  base: "/yuumisang",
   trailingSlash: "always",
   integrations: [
     tailwind(
